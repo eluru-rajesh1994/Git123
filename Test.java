f84392636d9e8a071db3f448ac68fb134547d632
rajesh <eluru.rajesh1994@gmail.com>
2026-01-03 05:50:42 +0530

    first changes here

--- Test.java
+++ Test.java
@@ -1,5 +1,17 @@
-public class Test{
+
+public class Test {
+
     public static void main(String[] args) {
         System.err.println("Hello Rajesh");
+        try {
+            demo();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void demo() throws Exception {
+
+        throw new Exception("Failed");
     }
 }
